7a09c9151abfe7b92ad11dfef621476c49c7c470
dianepristine <dianepristine01@gmail.com>
2023-10-14 14:25:58 -0700

    workshop 1 assignment

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import CampsitesList from './features/campsites/CampsitesList';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      hello nucamp!
-    </div>
-  );
+    return (
+        <div className='App'>
+            <Header />
+            <CampsitesList />
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
